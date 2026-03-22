9a116eb162ae73beb4950d7b57ddfe22a4103979
doorstepgamers <hello@doorstepgamers.co.uk>
2026-03-22 22:38:52 +0000

    Add files via upload

--- raspberry-pi/start-production.js
+++ raspberry-pi/start-production.js
@@ -30,8 +30,8 @@ server.listen(PORT, '0.0.0.0', () => {
 });
 
 function getLocalIP() {
-  const { networkInterfaces } = await import('os');
-  const nets = networkInterfaces();
+  const os = require('os');
+  const nets = os.networkInterfaces();
   for (const name of Object.keys(nets)) {
     for (const net of nets[name]) {
       if (net.family === 'IPv4' && !net.internal) {
